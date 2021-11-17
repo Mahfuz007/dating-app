200a45cf43b6723c18fc149aedfbf684603e4e0c
Mahfuz <aamahfuz2@gmail.com>
2021-11-17 16:54:50 +0600

    added home component

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
 import { AccountService } from "./_services/account.service";
 import { User } from "./_models/user";
 
@@ -12,25 +11,10 @@ export class AppComponent implements OnInit {
   title = "The Dating App";
   users: any;
 
-  constructor(
-    private http: HttpClient,
-    private accountService: AccountService
-  ) {}
+  constructor(private accountService: AccountService) {}
 
   ngOnInit() {
-    this.getUsers();
     this.setCurrentUser();
-  }
-
-  getUsers() {
-    this.http.get("https://localhost:5001/api/users").subscribe(
-      (response) => {
-        this.users = response;
-      },
-      (error) => {
-        console.log(error);
-      }
-    );
   }
 
   setCurrentUser() {
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -7,9 +7,10 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { NavComponent } from "./nav/nav.component";
 import { ReactiveFormsModule } from "@angular/forms";
 import { BsDropdownConfig, BsDropdownModule } from "ngx-bootstrap/dropdown";
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [AppComponent, NavComponent],
+  declarations: [AppComponent, NavComponent, HomeComponent],
   imports: [
     BrowserModule,
     HttpClientModule,
