af43c591e84600af3e024629921b8af006abdf43
Mahfuz <aamahfuz2@gmail.com>
2021-11-20 00:59:05 +0600

    added new components and router

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -7,17 +7,34 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { NavComponent } from "./nav/nav.component";
 import { ReactiveFormsModule } from "@angular/forms";
 import { BsDropdownConfig, BsDropdownModule } from "ngx-bootstrap/dropdown";
-import { HomeComponent } from './home/home.component';
-import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from "./home/home.component";
+import { RegisterComponent } from "./register/register.component";
+import { MemberListComponent } from "./members/member-list/member-list.component";
+import { MemberDetailsComponent } from "./members/member-details/member-details.component";
+import { ListsComponent } from "./lists/lists.component";
+import { MatchesComponent } from "./matches/matches.component";
+import { AppRoutingModule } from "./app-routing.module";
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
-  declarations: [AppComponent, NavComponent, HomeComponent, RegisterComponent],
+  declarations: [
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    RegisterComponent,
+    MemberListComponent,
+    MemberDetailsComponent,
+    ListsComponent,
+    MatchesComponent,
+    MessagesComponent,
+  ],
   imports: [
     BrowserModule,
     HttpClientModule,
     BrowserAnimationsModule,
     ReactiveFormsModule,
     BsDropdownModule.forRoot(),
+    AppRoutingModule,
   ],
   providers: [BsDropdownConfig],
   bootstrap: [AppComponent],
