8d52ab143de9659f6c8698afe67fec3f2b9e63c6
Mahfuz <aamahfuz2@gmail.com>
2021-12-12 23:12:05 +0600

    member service added

--- client/src/app/lists/lists.component.ts
+++ client/src/app/lists/lists.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Member } from "../_models/member";
+import { MemberService } from "../_services/member.service";
 
 @Component({
-  selector: 'app-lists',
-  templateUrl: './lists.component.html',
-  styleUrls: ['./lists.component.css']
+  selector: "app-lists",
+  templateUrl: "./lists.component.html",
+  styleUrls: ["./lists.component.css"],
 })
 export class ListsComponent implements OnInit {
+  memberList: Member[] = [];
 
-  constructor() { }
+  constructor(private memberService: MemberService) {}
 
   ngOnInit() {
+    this.loadMembers();
   }
 
+  loadMembers() {
+    this.memberService.getMembers().subscribe((res) => {
+      this.memberList = res;
+    });
+  }
 }
