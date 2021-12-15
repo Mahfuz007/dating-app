be05c40a5d91b7f33746330f1c278f186e48adaa
Mahfuz <aamahfuz2@gmail.com>
2021-12-15 23:36:48 +0600

    member details page style

--- client/src/app/_modules/shared.module.ts
+++ client/src/app/_modules/shared.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from "@angular/common";
 import { BsDropdownModule } from "ngx-bootstrap/dropdown";
 import { AppRoutingModule } from "../app-routing.module";
 import { ToastrModule } from "ngx-toastr";
+import { TabsModule, TabsetConfig } from "ngx-bootstrap/tabs";
 
 @NgModule({
   declarations: [],
@@ -13,7 +14,9 @@ import { ToastrModule } from "ngx-toastr";
     ToastrModule.forRoot({
       positionClass: "toast-bottom-right",
     }),
+    TabsModule.forRoot(),
   ],
-  exports: [BsDropdownModule, AppRoutingModule, ToastrModule],
+  exports: [BsDropdownModule, AppRoutingModule, ToastrModule, TabsModule],
+  providers: [TabsetConfig],
 })
 export class SharedModule {}
