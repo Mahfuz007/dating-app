e51967cf5d149f6b5619b698d64cf03777453fae
Mahfuz <aamahfuz2@gmail.com>
2021-12-24 23:52:06 +0600

    added loading interceptor

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -17,7 +17,9 @@ import { MessagesComponent } from "./messages/messages.component";
 import { SharedModule } from "./_modules/shared.module";
 import { MemberCardComponent } from "./members/member-card/member-card.component";
 import { JwtInterceptor } from "./_interceptors/jwt.interceptor";
-import { MemberEditComponent } from './members/member-edit/member-edit.component';
+import { MemberEditComponent } from "./members/member-edit/member-edit.component";
+import { LoadingInterceptor } from "./_interceptors/loading.interceptor";
+import { NgxSpinnerModule } from "ngx-spinner";
 
 @NgModule({
   declarations: [
@@ -38,12 +40,18 @@ import { MemberEditComponent } from './members/member-edit/member-edit.component
     BrowserAnimationsModule,
     ReactiveFormsModule,
     SharedModule,
+    NgxSpinnerModule,
   ],
   providers: [
     BsDropdownConfig,
     {
       provide: HTTP_INTERCEPTORS,
       useClass: JwtInterceptor,
+      multi: true,
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: LoadingInterceptor,
       multi: true,
     },
   ],
