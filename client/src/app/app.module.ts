9430487198fa5fc8f3c4675b60ca621817928448
Mahfuz <aamahfuz2@gmail.com>
2021-12-29 23:56:36 +0600

    upload image functionality from client side

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -20,7 +20,8 @@ import { JwtInterceptor } from "./_interceptors/jwt.interceptor";
 import { MemberEditComponent } from "./members/member-edit/member-edit.component";
 import { LoadingInterceptor } from "./_interceptors/loading.interceptor";
 import { NgxSpinnerModule } from "ngx-spinner";
-import { PhotoEditComponent } from './members/photo-edit/photo-edit.component';
+import { PhotoEditComponent } from "./members/photo-edit/photo-edit.component";
+import { ImageUploaderModule } from "ngx-image-uploader";
 
 @NgModule({
   declarations: [
@@ -43,6 +44,7 @@ import { PhotoEditComponent } from './members/photo-edit/photo-edit.component';
     ReactiveFormsModule,
     SharedModule,
     NgxSpinnerModule,
+    ImageUploaderModule,
   ],
   providers: [
     BsDropdownConfig,
--- client/src/app/members/photo-edit/photo-edit.component.ts
+++ client/src/app/members/photo-edit/photo-edit.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { environment } from "./../../../environments/environment";
+import { Component, Input, OnInit } from "@angular/core";
+import { Member } from "src/app/_models/member";
+import { ImageUploaderOptions, FileQueueObject } from "ngx-image-uploader";
+import { AccountService } from "src/app/_services/account.service";
+import { User } from "src/app/_models/user";
+import { take } from "rxjs/operators";
 
 @Component({
-  selector: 'app-photo-edit',
-  templateUrl: './photo-edit.component.html',
-  styleUrls: ['./photo-edit.component.css']
+  selector: "app-photo-edit",
+  templateUrl: "./photo-edit.component.html",
+  styleUrls: ["./photo-edit.component.css"],
 })
 export class PhotoEditComponent implements OnInit {
+  @Input("member") member: Member;
+  user: User;
+  baseUrl = environment.apiUrl;
+  options: ImageUploaderOptions;
 
-  constructor() { }
+  constructor(private accountService: AccountService) {}
 
-  ngOnInit() {
+  onUpload(file: FileQueueObject) {
+    console.log(file.response);
   }
 
+  ngOnInit() {
+    this.accountService.currentUser$.pipe(take(1)).subscribe((user) => {
+      this.user = user;
+      this.setOptions();
+    });
+  }
+
+  setOptions() {
+    this.options = {
+      thumbnailHeight: 220,
+      thumbnailWidth: 320,
+      uploadUrl: this.baseUrl + "users/add-photo",
+      allowedImageTypes: ["image/png", "image/jpeg"],
+      maxImageSize: 3,
+      authToken: "Bearer " + this.user.token,
+      autoUpload: false,
+    };
+  }
 }
