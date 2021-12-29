17dea664c6dc88859564c80b33c4ad2c176b94cf
Mahfuz <aamahfuz2@gmail.com>
2021-12-29 22:18:59 +0600

    photo-edit component added

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -20,6 +20,7 @@ import { JwtInterceptor } from "./_interceptors/jwt.interceptor";
 import { MemberEditComponent } from "./members/member-edit/member-edit.component";
 import { LoadingInterceptor } from "./_interceptors/loading.interceptor";
 import { NgxSpinnerModule } from "ngx-spinner";
+import { PhotoEditComponent } from './members/photo-edit/photo-edit.component';
 
 @NgModule({
   declarations: [
@@ -33,6 +34,7 @@ import { NgxSpinnerModule } from "ngx-spinner";
     MessagesComponent,
     MemberCardComponent,
     MemberEditComponent,
+    PhotoEditComponent,
   ],
   imports: [
     BrowserModule,
