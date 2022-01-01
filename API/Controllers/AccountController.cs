0879d9b3402612ac6e0f98be949ee4a20fd7601b
Mahfuz <aamahfuz2@gmail.com>
2022-01-01 22:58:40 +0600

    return photoUrl in login response

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -47,7 +47,9 @@ namespace API.Controllers
         [HttpPost("login")]
         public async Task<ActionResult<UserDto>> Login (LoginDto loginDto)
         {
-            var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
+            var user = await _context.Users
+                .Include( p => p.Photos)
+                .SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
 
             if (user == null) return Unauthorized("Invalid Username or Password");
 
@@ -63,7 +65,8 @@ namespace API.Controllers
             return new UserDto
             {
                 Username = user.UserName,
-                token = _tokenService.CreateToken(user)
+                token = _tokenService.CreateToken(user),
+                PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url
             };
         }
 
