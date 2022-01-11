ef50d00156f7c5f78c99f0d89062b4f1db9c983c
Mahfuz <aamahfuz2@gmail.com>
2022-01-11 22:40:12 +0600

    resuable text input field

--- client/src/app/_forms/input-field/input-field.component.ts
+++ client/src/app/_forms/input-field/input-field.component.ts
@@ -20,39 +20,12 @@ import {
   templateUrl: "./input-field.component.html",
   styleUrls: ["./input-field.component.css"],
 })
-export class InputFieldComponent implements ControlValueAccessor, OnInit {
+export class InputFieldComponent implements ControlValueAccessor {
   @Input() type: string = "text";
   @Input() label: string = "name";
 
-  errorMessages: any;
-  errors: any = [];
-
   constructor(@Self() @Optional() public ngControl: NgControl) {
     this.ngControl.valueAccessor = this;
-  }
-  ngOnInit(): void {
-    console.log("dd = ", this.ngControl.control);
-    // this.setErrorMessage();
-    this.ngControl.control.valueChanges.subscribe(() => {
-      console.log("error before  = ", this.ngControl.control.errors);
-      if (this.ngControl.control.errors) {
-        this.errors = Object.entries(this.ngControl.control.errors);
-        console.log("error = ", this.errors);
-      }
-    });
-  }
-
-  setErrorMessage() {
-    this.errorMessages.set("required", () => `This field is required`);
-    this.errorMessages.set(
-      "minlength",
-      () => `${this.label} must be at least three Characters`
-    );
-    this.errorMessages.set(
-      "maxlength",
-      () => `${this.label} must be at most Eight Characters`
-    );
-    this.errorMessages.set("isMatching", () => `Password does not match`);
   }
 
   writeValue(value: any): void {}
