ecc8a188f09ec7281d82b71651bbb1809d720d6c
Mahfuz <aamahfuz2@gmail.com>
2022-02-27 23:43:10 +0600

    added filter ui

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -7,6 +7,7 @@ import { Member } from "../../_models/member";
 import { MemberService } from "../../_services/member.service";
 import { UserParams } from "src/app/_models/userParams";
 import { take } from 'rxjs/operators';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: "app-member-list",
@@ -19,7 +20,14 @@ export class MemberListComponent implements OnInit {
   userParams: UserParams;
   user: User;
 
-  constructor(private memberService: MemberService, private accountService: AccountService) {
+  genderList = [{value: 'male' , display: "Males"}, { value: 'female' , display: "Females"}];
+
+  filtersForm: FormGroup;
+
+  constructor(private memberService: MemberService,
+     private accountService: AccountService,
+     private fb: FormBuilder
+     ) {
     this.accountService.currentUser$.pipe(take(1)).subscribe((user => {
       this.user = user;
       this.userParams = new UserParams(user);
@@ -27,7 +35,16 @@ export class MemberListComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.initForm();
     this.loadMembers();
+  }
+
+  initForm() {
+    this.filtersForm = this.fb.group({
+      AgeFrom: [18],
+      AgeTo: [99],
+      Gender: [this.user.gender == 'male'? 'female' : 'male']
+    })
   }
 
   loadMembers() {
@@ -41,4 +58,24 @@ export class MemberListComponent implements OnInit {
     this.userParams.pageNumber = $event.page;
     this.loadMembers();
   }
-}
+
+  applyFilter() {
+    this.setParamsValue()
+    this.loadMembers();
+  }
+
+  setParamsValue() {
+    this.userParams.minAge = this.filtersForm.value.AgeFrom;
+    this.userParams.maxAge = this.filtersForm.value.AgeTo;
+    this.userParams.gender = this.filtersForm.value.Gender;
+  }
+
+  resetFilter() {
+    this.filtersForm.get('AgeFrom').setValue(18);
+    this.filtersForm.get('AgeTo').setValue(99);
+    this.filtersForm.get('Gender').setValue(this.user.gender == 'male'? 'female' : 'male');
+
+    this.setParamsValue();
+    this.loadMembers();
+  }
+ }
